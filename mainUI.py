0d032407605085cb7e6499283d2f295e821f4496
ssttyy123 <1359821373@qq.com>
2023-03-09 19:18:32 +0800

    将tkinterUI库改为PyQt进行编写，使得多样性增加，便于添加更多组件操作

--- mainUI.py
+++ mainUI.py
@@ -1,15 +1,6 @@
-from tkinter import *
-from tkinter import ttk
-import ctypes
+from PyQt6.QtWidgets import *
+import sys
 import loginin
-
-
-def center_win(root, width, height):
-    size = '%dx%d+%d+%d' % (width, height, (root.winfo_screenwidth() - width) / 2,
-                            (root.winfo_screenheight() - height) / 2)
-    root.geometry(size)
-    root.update()
-    root.resizable(0, 0)
 
 
 def createsis(userid, pas, operator):
@@ -18,34 +9,61 @@ def createsis(userid, pas, operator):
     logina.on_login()
 
 
-ctypes.windll.shcore.SetProcessDpiAwareness(1)
-ScaleFactor = ctypes.windll.shcore.GetScaleFactorForDevice(0)
-mainwindow = Tk()
-# 居中
-# center_win(mainwindow, 410, 150)
-mainwindow.title("LoginSetting")
-mainwindow.tk.call('tk', 'scaling', ScaleFactor/75)
-# 学号输入
-text_userid = Label(mainwindow, text="Student number:")
-text_userid.grid(row=0, column=0, padx=(10, 2), pady=10)
-enter_userid = Entry(mainwindow, bd=5)
-enter_userid.grid(row=0, column=1, padx=(0, 10), pady=(20, 10))
-# 密码输入
-text_pas = Label(mainwindow, text="Password:")
-text_pas.grid(row=1, column=0, padx=(10, 2), pady=10)
-enter_pas = Entry(mainwindow, bd=5)
-enter_pas.config(show='*')
-enter_pas.grid(row=1, column=1, padx=(0, 10), pady=10)
-# 运营商选择
-text_operator = Label(mainwindow, text="Select operator:")
-text_operator.grid(row=3, column=0, padx=(10, 0), pady=5)
-operator_ddl = ttk.Combobox(mainwindow, width=15)
-operator_ddl.grid(row=3, column=1, padx=(0, 10), pady=5)
-operator_ddl['value'] = ("cmcc", "chinanet")
-operator_ddl.config(state='readonly')
-operator_ddl.current(0)
-# 提交按钮
-sub_button = Button(mainwindow, text="submit", height=1,
-                    command=lambda: createsis(enter_userid.get(), enter_pas.get(), operator_ddl.get()))
-sub_button.grid(row=4, column=1, padx=(0, 10), pady=(5, 10))
-mainwindow.mainloop()
+class Mainwindow(QWidget):
+    def __init__(self):
+        super(Mainwindow, self).__init__()
+
+    def initui(self):
+        # 学号输入
+        text_userid = QLabel('Student number:')
+        enter_userid = QLineEdit()
+
+        # 密码输入
+        text_pas = QLabel('Password:')
+        enter_pas = QLineEdit()
+        # enter_pas.setEchoMode(QLineEdit.Password)
+
+        # 运营商选择
+        text_oper = QLabel('Select operator:')
+        op_cbb = QComboBox()
+        op_cbb.addItem("cmcc")
+        op_cbb.addItem("chinanet")
+
+        # 提交按钮
+        subbutton = QPushButton("submit")
+
+        # 标签列
+        vboxl = QVBoxLayout()
+        vboxl.addWidget(text_userid)
+        vboxl.addWidget(text_pas)
+        vboxl.addWidget(text_oper)
+
+        # 输入组件列
+        vboxaction = QVBoxLayout()
+        vboxaction.addWidget(enter_userid)
+        vboxaction.addWidget(enter_pas)
+        vboxaction.addWidget(op_cbb)
+
+        # 表单填写组
+        hbox = QHBoxLayout()
+        hbox.addLayout(vboxl)
+        hbox.addLayout(vboxaction)
+
+        # 整体布局
+        vboxall = QVBoxLayout()
+        vboxall.addLayout(hbox)
+        vboxall.addWidget(subbutton)
+
+        self.setWindowTitle("LoginSetting")
+        self.setLayout(vboxall)
+        self.setFixedSize(257, 125)
+        self.show()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    mainui = Mainwindow()
+    mainui.initui()
+
+    sys.exit(app.exec())
