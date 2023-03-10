8fb2f37cd10ff63c10bcc8d6d958e28c443bcdd9
ssttyy123 <1359821373@qq.com>
2023-03-10 22:51:58 +0800

    新增开机自启选项，在启动程序时自动连接一次

--- logindata.py
+++ logindata.py
@@ -4,8 +4,8 @@ import json
 
 class usersdata:
     def __init__(self):
-        if not os.path.exists(".\\userdata.json"):
-            self.fp = open(".\\userdata.json", 'w')
+        if not os.path.exists(r".\userdata.json"):
+            self.fp = open(r".\userdata.json", 'w')
             basestruct = {'userdata': [
                 {"id": "templateUser",
                  "pas": "123456",
@@ -13,14 +13,16 @@ class usersdata:
             ],
                 'default': {"id": "templateUser",
                             "pas": "123456",
-                            "ope": 0}
+                            "ope": 0,
+                            'auto_start': 0}
             }
+            # auto_start(0/1)不开机自启/开机自启
             json.dump(basestruct, self.fp, indent=4)
             self.fp.close()
-            self.fp = open(".\\userdata.json")
+            self.fp = open(r".\userdata.json")
             self.userlist = json.load(self.fp)
         else:
-            self.fp = open(".\\userdata.json")
+            self.fp = open(r".\userdata.json")
             self.userlist = json.load(self.fp)
 
     def writenewuser(self, userid, pas, ope):
@@ -28,11 +30,11 @@ class usersdata:
                        "pas": pas,
                        "ope": ope}
         self.fp.close()
-        self.fp = open(".\\userdata.json", 'w')
+        self.fp = open(r".\userdata.json", 'w')
         self.userlist['userdata'].append(newuserdata)
         json.dump(self.userlist, self.fp, indent=4)
         self.fp.close()
-        self.fp = open(".\\userdata.json")
+        self.fp = open(r".\userdata.json")
         self.userlist = json.load(self.fp)
 
     def readdata(self, userid):
@@ -40,3 +42,16 @@ class usersdata:
 
     def rutdefaultuesr(self):
         return self.userlist['default']
+
+    def changefault(self, userid, pas, ope, auto_start):
+        newuserdata = {"id": userid,
+                       "pas": pas,
+                       "ope": ope,
+                       'auto_start': auto_start}
+        self.fp.close()
+        self.fp = open(r".\userdata.json", 'w')
+        self.userlist['default'] = newuserdata
+        json.dump(self.userlist, self.fp, indent=4)
+        self.fp.close()
+        self.fp = open(r".\userdata.json")
+        self.userlist = json.load(self.fp)
--- mainUI.py
+++ mainUI.py
@@ -1,8 +1,13 @@
+import win32api
+import winreg
+import win32con
 from PyQt6 import QtGui, QtCore
 from PyQt6.QtCore import QCoreApplication
 from PyQt6.QtGui import QAction
 from PyQt6.QtWidgets import *
+
 import sys
+import os
 
 import loginin
 import logindata
@@ -11,9 +16,30 @@ import logindata
 def createsis(mainwindow, userid, pas, operator):
     # 创建会话类
     logina = loginin.Loginof(userid, pas, operator)
-    logina.on_login()
-    mainwindow.showMinimized()
-    mainwindow.setWindowFlag(QtCore.Qt.WindowType.FramelessWindowHint)
+    rt = logina.on_login()
+    # 1 IP终端已经在线
+    # 2 验证失败
+    # 3 登录成功
+    return rt
+
+
+def auto_start(situ):
+    # (0,1,2)存在键，不存在键，权限不足
+    reg_path = r'Software\Microsoft\Windows\CurrentVersion\Run'
+    execpath = os.path.realpath(sys.argv[0])
+    key = winreg.OpenKey(win32con.HKEY_CURRENT_USER, reg_path, 0, win32con.KEY_ALL_ACCESS)
+    try:
+        rt = winreg.QueryValueEx(key, 'login_school')[1]
+        # 存在键
+        if situ == "close":
+            if rt == 1:
+                win32api.RegDeleteValue(key, "login_school")
+                win32api.RegCloseKey(key)
+    except FileNotFoundError as e:
+        # 不存在键
+        if situ == "open":
+            win32api.RegSetValueEx(key, "login_school", 0, win32con.REG_SZ, execpath)
+            win32api.RegCloseKey(key)
 
 
 class Mainwindow(QWidget):
@@ -37,6 +63,10 @@ class Mainwindow(QWidget):
         # 提交按钮
         self.subbutton = QPushButton("submit")
 
+        # 开机自启选择框
+        self.text_sta = QLabel('auto-start:')
+        self.startCheckbox = QCheckBox()
+
         # 用户历史数据
         '''{"id": "templateUser",
                  "pas": "123456",
@@ -45,6 +75,7 @@ class Mainwindow(QWidget):
         self.enter_userid.setText(self.database.rutdefaultuesr()['id'])
         self.enter_pas.setText(self.database.rutdefaultuesr()['pas'])
         self.op_cbb.setCurrentIndex(self.database.rutdefaultuesr()['ope'])
+        self.startCheckbox.setChecked(self.database.rutdefaultuesr()['auto_start'])
 
     def initui(self):
         # 标签列
@@ -64,14 +95,21 @@ class Mainwindow(QWidget):
         hbox.addLayout(vboxl)
         hbox.addLayout(vboxaction)
 
+        # 提交按钮，开机自启选项行
+        ablehbox = QHBoxLayout()
+        ablehbox.addWidget(self.text_sta)
+        ablehbox.addWidget(self.startCheckbox)
+        ablehbox.addStretch(1)
+        ablehbox.addWidget(self.subbutton)
+
         # 整体布局
         vboxall = QVBoxLayout()
         vboxall.addLayout(hbox)
-        vboxall.addWidget(self.subbutton)
+        vboxall.addLayout(ablehbox)
 
         self.setWindowTitle("LoginSetting")
         self.setLayout(vboxall)
-        self.setFixedSize(257, 125)
+        # self.setFixedSize(257, 125)
         self.show()
 
 
@@ -83,6 +121,7 @@ class systray(QSystemTrayIcon):
     def __init__(self, mainwindow, parent=None):
         super(QSystemTrayIcon, self).__init__(parent)
         self.menu = None
+        self.autoflag = 1
         self.actionset = QAction("Setting", self)
         self.actionquit = QAction("Quit", self)
         self.actionset.triggered.connect(self.showui)
@@ -90,9 +129,24 @@ class systray(QSystemTrayIcon):
         self.mainwindow = mainwindow
         self.creatMenu()
         # 提交按钮
-        mainwindow.subbutton.clicked.connect(lambda: createsis(mainwindow, mainwindow.enter_userid.text(),
-                                                               mainwindow.enter_pas.text(),
-                                                               mainwindow.op_cbb.currentText()))
+        mainwindow.subbutton.clicked.connect(lambda: self.onclicksub())
+
+    def onclicksub(self):
+        createsis(self.mainwindow, self.mainwindow.enter_userid.text(),
+                  self.mainwindow.enter_pas.text(),
+                  self.mainwindow.op_cbb.currentText())
+        if self.mainwindow.startCheckbox.isChecked():
+            auto_start("open")
+            self.autoflag = 1
+        elif not self.mainwindow.startCheckbox.isChecked():
+            auto_start("close")
+            self.autoflag = 0
+        self.mainwindow.database.changefault(self.mainwindow.enter_userid.text(),
+                                             self.mainwindow.enter_pas.text(),
+                                             self.mainwindow.op_cbb.currentIndex(),
+                                             self.autoflag)
+        self.mainwindow.showMinimized()
+        self.mainwindow.setWindowFlags(QtCore.Qt.WindowType.SplashScreen | QtCore.Qt.WindowType.FramelessWindowHint)
 
     def creatMenu(self):
         self.menu = QMenu()
@@ -100,8 +154,8 @@ class systray(QSystemTrayIcon):
         self.menu.addAction(self.actionquit)
         self.setContextMenu(self.menu)
 
-        self.setIcon(QtGui.QIcon(".\\res\\connect.ico"))
-        self.mainwindow.setWindowFlag(QtCore.Qt.WindowType.FramelessWindowHint)
+        self.setIcon(QtGui.QIcon(r".\res\connect.ico"))
+        self.mainwindow.setWindowFlags(QtCore.Qt.WindowType.SplashScreen | QtCore.Qt.WindowType.FramelessWindowHint)
 
     def showui(self):
         self.mainwindow.showNormal()
@@ -113,7 +167,9 @@ if __name__ == '__main__':
 
     mainui = Mainwindow()
     mainui.initui()
+    mainui.setWindowFlags(QtCore.Qt.WindowType.SplashScreen | QtCore.Qt.WindowType.FramelessWindowHint)
     tray = systray(mainui)
+    tray.onclicksub()
     tray.show()
 
     sys.exit(app.exec())
