531a889c579848ae081f78595caa8f1b12c853dd
Stary112 <1359821373@qq.com>
2023-09-22 19:26:54 +0800

    wifi 登录功能修复 先已可以正常登录

--- loginin.py
+++ loginin.py
@@ -2,12 +2,15 @@ import json
 import re
 
 import requests
+from lxml import etree
 
 
 class Loginof(object):
     wlanip = ""
 
     urlM = "https://p.njupt.edu.cn:802/eportal/portal/login/"
+
+    login_home_url = "https://p.njupt.edu.cn/a79.htm"
 
     params = {
         "callback": "dr1003",
@@ -43,17 +46,43 @@ class Loginof(object):
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
                       "Chrome/112.0.0.0 Safari/537.36 Edg/112.0.1722.46"}
 
+    home_page_headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                      "AppleWebKit/537.36 (KHTML, like Gecko) "
+                      "Chrome/117.0.0.0 Safari/537.36 Edg/117.0.2045.36",
+        "Referer": "https://p.njupt.edu.cn/a79.htm"
+    }
+
     opelist = {'中国移动': 'cmcc',
                '中国联通': 'njxy'}
+
+    ipv4_xpath = "//head/script[1]/text()"
 
     def __init__(self, schoolid, pas, operator):
         self.params["user_account"] = ",0," + schoolid + "@" + self.opelist[operator]
         self.params["user_password"] = pas
 
+    def get_wlan_ipv4(self):
+        try:
+            try:
+                home_page_response = requests.get(url=self.login_home_url, headers=self.home_page_headers)
+                home_text = home_page_response.text
+                _elem = etree.HTML(home_text)
+                ipv4_script_str = str(_elem.xpath(self.ipv4_xpath)[0].encode('utf-8').decode('utf-8'))
+                reResult = re.findall(r"v46ip='(.+?)'", ipv4_script_str)
+                self.params["wlan_user_ip"] = reResult[0]
+                # print(reResult[0])
+            except requests.exceptions.SSLError:
+                print("no network")
+
+        except requests.exceptions.SSLError:
+            print("no network")
+
     def on_login(self):
         # proxies = {'http': None,
         #            'https': None}
         rt = ()
+        self.get_wlan_ipv4()
         try:
             response = requests.get(url=self.urlM, params=self.params, headers=self.header)
             json_str = re.findall(r"dr1003\((.+?)\);", response.text)
